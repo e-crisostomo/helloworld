4dec8c1f8aaeef07ca46193ba80b5f034a71950b
eneng <ednel.crisostomo@protonmail.com>
2021-02-06 11:57:00 -0500

    removed line 17

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-#this is from laptop
+
 #this is from laptop again.
 #this is a local edit
